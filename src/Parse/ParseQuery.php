a48268dd5ce77422a951d145c02dbcfc314d6591
pimpreneil <contact@arnaud-lejosne.com>
2015-08-07 10:56:39 +0200

    Add a direct regex filter

--- src/Parse/ParseQuery.php
+++ src/Parse/ParseQuery.php
@@ -231,6 +231,22 @@ class ParseQuery
 
         return $this;
     }
+    
+    /**
+     * Add a constraint to the query that requires a particular key's value
+     * determined by a regex
+     *
+     * @param string $key   The key to check.
+     * @param string $value The regex to match against
+     *
+     * @return ParseQuery Returns this query, so you can chain this call.
+     */
+    public function like($key, $value)
+    {
+        $this->addCondition($key, '$regex', $this->quote($value));
+
+        return $this;
+    }
 
     /**
      * Returns an associative array of the query constraints.
